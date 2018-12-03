51ef8e195ad1c5af0b794b02ff2ce8c22325ae36
hafpaf <haf@hafnium.me>
2018-12-03 22:50:07 +0100

    #more readability

--- fmod.py
+++ fmod.py
@@ -1,8 +1,11 @@
 import numpy
+#input
 number1 = int(input("number: ",))
 number2 = int(input("number: ",))
+
+#calculate
 modulo = numpy.fmod(number1,number2)
 modulo2 = number1 % number2
 
-#cmath libary vs python standard funtion
+#cmath libary vs python standard function
 print("fmod:",modulo,"vs", "%:",modulo2)
