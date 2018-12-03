04cc765a5ab8ccf014bc663c87c29f519dcdb42e
hafpaf <haf@hafnium.me>
2018-12-03 22:18:08 +0100

    input working

--- fmod.py
+++ fmod.py
@@ -1,7 +1,6 @@
 import numpy
-the_input = int(input(number1, number2))
-number1 = the_input[0]
-number2 = the_input[1]
+number1 = int(input("number: ",))
+number2 = int(input("number: ",))
 modulo = numpy.fmod(number1,number2)
 modulo2 = number1 % number2
-print(modulo, modulo2)
+print("fmod:",modulo,"vs", "%:",modulo2)
