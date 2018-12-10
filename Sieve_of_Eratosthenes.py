0ff0fc2e2009793bd68d67b188fc924c174e8f35
hafpaf <haf@hafnium.me>
2018-12-10 01:47:36 +0100

    more correct variable

--- Sieve_of_Eratosthenes.py
+++ Sieve_of_Eratosthenes.py
@@ -4,36 +4,37 @@
 # https://stackoverflow.com/a/568618/
 
 def gen_primes():
-    """ Generate an infinite sequence of prime numbers.
+    """
+    Generate an infinite sequence of prime numbers.
     """
     # Maps composites to primes witnessing their compositeness.
     # This is memory efficient, as the sieve is not "run forward"
     # indefinitely, but only as long as required by the current
     # number being tested.
     #
-    known_prime = {}
+    prime_sequence = {}
 
     # The running integer that's checked for primeness
     check_prime = 2
 
     while True:
-        if check_prime not in known_prime:
+        if check_prime not in prime_sequence:
             # check_prime is a new prime.
             # Yield it and mark its first multiple that isn't
             # already marked in previous iterations
             #
             yield check_prime
-            known_prime[check_prime * check_prime] = [check_prime]
+            prime_sequence[check_prime * check_prime] = [check_prime]
         else:
-            # check_prime is composite. known_prime[check_prime] is the list of primes that
+            # check_prime is composite. prime_sequence[check_prime] is the list of primes that
             # divide it. Since we've reached check_prime, we no longer
             # need it in the map, but we'll mark the next
             # multiples of its witnesses to prepare for larger
             # numbers
             #
-            for p in known_prime[check_prime]:
-                known_prime.setdefault(p + check_prime, []).append(p)
-            del known_prime[check_prime]
+            for p in prime_sequence[check_prime]:
+                prime_sequence.setdefault(p + check_prime, []).append(p)
+            del prime_sequence[check_prime]
 
         check_prime += 1
 
